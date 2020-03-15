725ec0d9b6167f71dadb8e3f86df7e8872a3870d
QwertyFusion <60546350+QwertyFusion@users.noreply.github.com>
2020-03-15 23:35:37 +0530

    Update greetings.js

--- commands/greetings.js
+++ commands/greetings.js
@@ -3,15 +3,16 @@ module.exports = {
     description: "Welcomes new members!",
     execute(member, RichEmbed, colour)
     {
-        const channel = member.guild.channels.find(channel => channel.name === 'welcome');
-        const channel2= member.guild.channels.find(channel => channel.name === 'uniform');
+        const channel = member.guild.channels.find(channel => channel.name === 'main-chat');
+        const channel2= member.guild.channels.find(channel => channel.name === 'new-uniform');
         const channel3= member.guild.channels.find(channel => channel.name === 'event-information');
-        const channel4= member.guild.channels.find(channel => channel.name === 'how-to-battle');
+        const channel4= member.guild.channels.find(channel => channel.name === 'games-information');
         channel.send(`${member} just now joined **The Tree Cult**! Welcome! You are the ${member.guild.memberCount} member!
         🌱 Check out Event Information → ${channel3}
         🌱 Check out the uniform → ${channel2}
-        🌱 Check out how to battle → ${channel4}
-        🌿 MAKE SURE YOU ATTEND THE HUGE BATTLES FOR HUGE PRIZES AND PROMOTION`);      
+        🌱 Check out The Games → ${channel4}
+        🌿 MAKE SURE YOU ATTEND THE HUGE BATTLES FOR HUGE PRIZES AND PROMOTION
+        Visit our website at https://treecultofcpo.wordpress.com`);      
 
         const embed7 =new RichEmbed()
         .setTitle(`Hello I am Larry The Bulbasaur!🌱`)
