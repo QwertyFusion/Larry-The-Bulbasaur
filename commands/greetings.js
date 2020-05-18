da39acb159e8a2b8e21215d34def009f254fcdeb
QwertyFusion <60546350+QwertyFusion@users.noreply.github.com>
2020-05-18 22:13:53 +0530

    Update greetings.js

--- commands/greetings.js
+++ commands/greetings.js
@@ -3,15 +3,15 @@ module.exports = {
     description: "Welcomes new members!",
     execute(member, RichEmbed, colour)
     {
-        const channel = member.guild.channels.find(channel => channel.name === 'main-chat');
-        const channel2= member.guild.channels.find(channel => channel.name === 'uniform');
-        const channel3= member.guild.channels.find(channel => channel.name === 'event-infon');
+        const channel = member.guild.channels.find(channel => channel.name === 'living-room');
+        const channel2= member.guild.channels.find(channel => channel.name === '');
+        const channel3= member.guild.channels.find(channel => channel.name === '');
         const channel4= member.guild.channels.find(channel => channel.name === 'role-menu');
-        channel.send(`${member} just now joined **The Tree Cult**! Welcome to The Tree Cult Family! You are the ${member.guild.memberCount} member!\n🌱 Check out Event Information → ${channel3}\n🌱 Check out the uniform → ${channel2}\n🌱 Get Roles from Role Menu → ${channel4}\n🌿 MAKE SURE YOU ATTEND THE HUGE BATTLES FOR HUGE PRIZES AND PROMOTION\n🌿 Visit our website at **treecultofcpo.wordpress.com**`);      
+        channel.send(`${member} just now joined **The Tree Cult**! Welcome to The Tree Cult Family! You are the ${member.guild.memberCount} member!\n🌱 Get Roles from Role Menu → ${channel4}\n🌿 Visit our website at **treecultofcpo.wordpress.com**`);      
 
         const embed7 =new RichEmbed()
         .setTitle(`Hello I am Larry The Bulbasaur!🌱`)
-        .addField(`Welcome to Tree Cult Family!\nHave a great time here in The Tree Cult!`, `Feel free to invite your firends and play Club Penguin Official over here!\nStay with us and help the army to grow!🌿`)
+        .addField(`Welcome to Tree Cult Family!\nHave a great time here in The Tree Cult!`, `Feel free to invite your firends and play games over here!\nStay with us and help the community to grow!🌿`)
         .setColor(colour)
         member.send(embed7);
         
